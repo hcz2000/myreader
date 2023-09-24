668dedf5a30e877290dc78dca686c7c73514b591
hcz2000 <huangchangzhan@hotmail.com>
2023-09-24 18:52:15 +0800

    update package

--- app/src/main/java/com/zhan/myreader/ui/home/bookcase/BookcasePresenter.java
+++ app/src/main/java/com/zhan/myreader/ui/home/bookcase/BookcasePresenter.java
@@ -1,12 +1,18 @@
 package com.zhan.myreader.ui.home.bookcase;
 
 import android.annotation.SuppressLint;
+import android.app.LoaderManager;
 import android.content.Intent;
+import android.content.Loader;
 import android.os.Build;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.util.Log;
 import android.view.View;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.core.content.ContextCompat;
 import com.scwang.smartrefresh.layout.api.RefreshLayout;
 import com.scwang.smartrefresh.layout.listener.OnRefreshListener;
@@ -15,25 +21,31 @@ import com.zhan.myreader.base.BasePresenter;
 import com.zhan.myreader.callback.ResultCallback;
 import com.zhan.myreader.custom.DragSortGridView;
 import com.zhan.myreader.greendao.entity.Book;
+import com.zhan.myreader.greendao.entity.Chapter;
 import com.zhan.myreader.greendao.service.BookService;
 import com.zhan.myreader.ui.home.MainActivity;
+import com.zhan.myreader.ui.home.reader.BookLoader;
 import com.zhan.myreader.ui.search.SearchBookActivity;
+import com.zhan.myreader.util.TextHelper;
 import com.zhan.myreader.util.VibratorUtil;
 import com.zhan.myreader.webapi.BookApi;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 /**
  * Created by zhan on 2017/7/25.
  */
 
-public class BookcasePresenter extends BasePresenter {
+public class BookcasePresenter extends BasePresenter implements LoaderManager.LoaderCallbacks{
 
     private final BookcaseFragment mBookcaseFragment;
     private final ArrayList<Book> mBooks = new ArrayList<>();//书目数组
     private BookcaseDragAdapter mBookcaseAdapter;
     private final BookService mBookService;
     private MainActivity mMainActivity;
+    private LoaderManager loaderManager;
+
 
     @SuppressLint("HandlerLeak")
     private final Handler mHandler = new Handler() {
@@ -60,6 +72,7 @@ public class BookcasePresenter extends BasePresenter {
     @Override
     public void start() {
         mMainActivity = ((MainActivity) (mBookcaseFragment.getContext()));
+        loaderManager=mMainActivity.getLoaderManager();
         mBookcaseFragment.getContentView().setEnableRefresh(false);
         mBookcaseFragment.getContentView().setEnableHeaderTranslationContent(false);
         mBookcaseFragment.getContentView().setEnableLoadMore(false);
@@ -137,6 +150,13 @@ public class BookcasePresenter extends BasePresenter {
 
     private void initNoReadNum() {
         for (final Book book : mBooks) {
+            if(book.getChapterTotalNum()==0) {
+                Bundle args=new Bundle();
+                args.putString("BookId",book.getId());
+                int loaderid=Integer.parseInt(book.getId());
+                if(loaderManager.getLoader(loaderid)==null)
+                    loaderManager.initLoader(loaderid, args, this);
+            }
             BookApi.getNewChapterCount(book, new ResultCallback() {
                 @Override
                 public void onFinish(Object obj, int code) {
@@ -161,7 +181,6 @@ public class BookcasePresenter extends BasePresenter {
     }
 
     private void setThemeColor(int colorPrimary, int colorPrimaryDark) {
-//        mToolbar.setBackgroundResource(colorPrimary);
         mBookcaseFragment.getContentView().setPrimaryColorsId(colorPrimary, android.R.color.white);
         Objects.requireNonNull(mBookcaseFragment.getActivity()).getWindow()
             .setStatusBarColor(ContextCompat.getColor(Objects.requireNonNull(mBookcaseFragment.getContext()), colorPrimaryDark));
@@ -170,5 +189,27 @@ public class BookcasePresenter extends BasePresenter {
     @Override
     public void resume(){
         getData();
+    }
+
+    @Override
+    public Loader onCreateLoader(int id, @Nullable Bundle args) {
+        String bookid=args.getString("BookId");
+        for(Book book : mBooks){
+            if(book.getId().equals(bookid))
+                return new CatalogLoader(mMainActivity.getBaseContext(),book);
+        }
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader loader, Object data) {
+        Log.d("ReadPresenter","Download completed");
+        TextHelper.showText(((Book)data).getName() +" catalog downloaded");
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader loader) {
+        Log.d("ReadPresenter","Loader reseting");
+        //loaderManager.restartLoader(loader.getId(),null,this);;
     }
 }
--- app/src/main/java/com/zhan/myreader/ui/home/bookcase/CatalogLoader.java
+++ app/src/main/java/com/zhan/myreader/ui/home/bookcase/CatalogLoader.java
@@ -8,7 +8,7 @@ import com.zhan.myreader.base.application.MyApplication;
 import com.zhan.myreader.greendao.entity.Book;
 import com.zhan.myreader.greendao.entity.Chapter;
 import com.zhan.myreader.greendao.service.ChapterService;
-import com.zhan.myreader.ui.home.reader.ProgressListener;
+import com.zhan.myreader.greendao.service.BookService;
 import com.zhan.myreader.webapi.BookApi;
 
 import java.util.ArrayList;
@@ -16,19 +16,17 @@ import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
-public class CatalogLoader extends AsyncTaskLoader<List<Chapter>>{
+public class CatalogLoader extends AsyncTaskLoader<Book>{
+    private BookService mBookService;
     private ChapterService mChapterService;
     private Book mBook;
-    private int downloadedCount;
 
-    private List<ProgressListener> progressListeners;
 
     CatalogLoader(Context context, Book book) {
         super(context);
         mBook=book;
+        mBookService = new BookService();
         mChapterService = new ChapterService();
-        progressListeners=new ArrayList<ProgressListener>();
-        downloadedCount=0;
     }
 
     @Override
@@ -38,36 +36,14 @@ public class CatalogLoader extends AsyncTaskLoader<List<Chapter>>{
     }
 
     @Override
-    public List<Chapter> loadInBackground() {
-        List<Chapter> chapters=mChapterService.findBookAllChapterByBookId(mBook.getId());
-        CountDownLatch latch=new CountDownLatch(chapters.size());
-        for(Chapter chapter : chapters){
-            MyApplication.getApplication().newLoader(()->{
-                if(chapter.getContent()==null||chapter.getContent().equals("")) {
-                    loadChapter(chapter);
-                    mChapterService.updateChapter(chapter);
-                }
-                if(chapter.getContent()!=null){
-                    synchronized(this){
-                        downloadedCount++;
-                    }
-                    String progress=downloadedCount+"/"+chapters.size();
-                    for(ProgressListener listener:progressListeners)
-                        listener.notify(progress);
-                }
-                latch.countDown();
-            });
-        }
-        try {
-            latch.await(600, TimeUnit.SECONDS);
-        }catch(Exception e){
-            Log.d("BookLoader",e.getLocalizedMessage());
-        };
-        String progress=downloadedCount+"/"+chapters.size();
-        for(ProgressListener listener:progressListeners)
-            listener.notify(progress);
-        System.out.println("All download task completed");
-        return chapters;
+    public Book loadInBackground() {
+        List<Chapter> chapters = BookApi.getBookChapters(mBook);
+        int newTotal=chapters.get(chapters.size()-1).getNumber()+1;
+        mBook.setChapterTotalNum(newTotal);
+        mBook.setNoReadNum(chapters.size());
+        mBookService.updateEntity(mBook);
+        mChapterService.addChapters(chapters);
+        return mBook;
     }
     @Override
     protected void onStopLoading() {
@@ -75,23 +51,9 @@ public class CatalogLoader extends AsyncTaskLoader<List<Chapter>>{
     }
 
     @Override
-    public void deliverResult(List<Chapter> data) {
+    public void deliverResult(Book data) {
         if (isStarted()) {
             super.deliverResult(data);
         }
-    }
-
-    private void loadChapter(Chapter chapter){
-        String content= BookApi.getChapterContent(chapter);
-        if(content!=null){
-            chapter.setContent(content);
-            Log.d("BookLoader",chapter.getTitle()+" loaded");
-            System.out.println(chapter.getTitle()+" loaded");
-        }
-
-    }
-
-    public void registerProgressListener(ProgressListener listener){
-        progressListeners.add(listener);
     }
 }
